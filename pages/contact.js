8789a96fe8e03ed0638c150a851db61b8daec41b
prasantadebnath.2013@gmail.com <prasantadebnath.2013@gmail.com>
2022-03-02 23:50:41 +0530

    Prasanta: routing page changes done

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head'
+
+export default function contact() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
+      </Head>
+      <Link href="/post">
+          Go To Post Page
+      </Link>
+    </div>
+  )
+}
