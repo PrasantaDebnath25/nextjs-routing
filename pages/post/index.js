9a7f0690fd484f75bde088fb0a2e1e5360480ac7
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-08-03 13:45:10 +0530

    Formatting changes

--- pages/post/index.js
+++ pages/post/index.js
@@ -4,25 +4,25 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 
-export default function postFunction({posts}) {
+export default function postFunction({ posts }) {
   const router = useRouter()
   return (
     <ul>
       <h1>My Homepage</h1>
-       {/* <img src="/img4.jpg" alt="Vercel Logo" /> */}
-       {/* <Image src="/img4.jpg" format="jpg" alt="Logo" width="200" height="200"/> */}
-        {/* <Image src={img} alt="Logo" width={200} height={200} />  */}
-        { posts.map((post) => {
-            return(
-                <li key={post.id}>
-                    <h3> 
-                         <div onClick={() => router.push('/post/' +post.id)}> {post.title}  </div>
-                    </h3>
-                    <p>  {post.body} </p>
-                </li>
-            );
-           }
-        ) }
+      {/* <img src="/img4.jpg" alt="Vercel Logo" /> */}
+      {/* <Image src="/img4.jpg" format="jpg" alt="Logo" width="200" height="200"/> */}
+      {/* <Image src={img} alt="Logo" width={200} height={200} />  */}
+      {posts.map((post) => {
+        return (
+          <li key={post.id}>
+            <h3>
+              <div onClick={() => router.push('/post/' + post.id)}> {post.title}  </div>
+            </h3>
+            <p>  {post.body} </p>
+          </li>
+        );
+      }
+      )}
     </ul>
   )
 }
@@ -37,12 +37,12 @@ export default function postFunction({posts}) {
 //       }
 // }
 
-export async function getServerSideProps(){
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const posts = await res.json()
-    return {
-        props: {
-          posts,
-        } ,
-      }
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await res.json()
+  return {
+    props: {
+      posts,
+    },
+  }
 }
