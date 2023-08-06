b093ba37d64ef37971c6f12fcce6cb806c1bd4b3
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-08-06 14:11:56 +0530

    Formattings

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Link href="/contact" className={styles.main}>
         <a>Go to Contact Page</a>
       </Link>
-      
+
       <Link href="/post">
-          Go To Post Page
+        Go To Post Page
       </Link>
 
       <main className={styles.main}>
