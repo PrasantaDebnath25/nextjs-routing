7bba873ff006e74ee357cc266154645a004c7d0e
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-08-08 13:37:39 +0530

    Routing added

--- pages/contact.js
+++ pages/contact.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import Link from 'next/link';
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function contact() {
   return (
-    <div>
-      <Head>
+
+
+<div className={styles.container}>
+<Head>
         <title>My page title</title>
-        {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
         
       </Head>
+      
+      {/* <Link href="/contact" className={styles.main}>
+        <a>Go to Contact Page</a>
+      </Link>
+      
       <Link href="/post">
           Go To Post Page
-      </Link>
+      </Link> */}
 
-      <Link href="/post/1">
-          Go To Post Page for page 1
-      </Link>
+      <main className={styles.main}>
+
+        <div className={styles.grid}>
+          <a href="/post" className={styles.card}>
+            <h3>Post Page for page 1 &rarr;</h3>
+            <p>Go To Post Page for page 1</p>
+          </a>
+
+          <a href="/post/1" className={styles.card}>
+            <h3>Post Page &rarr;</h3>
+            <p>Go To Post Page</p>
+          </a>
+
+        </div>
+      </main>
+
     </div>
   )
 }
